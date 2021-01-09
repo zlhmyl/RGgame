cf1e2a496019ece99fceb160a3ea9d7da8ea9db2
fxf <2687235779@qq.com>
2021-01-09 11:20:43 +0800

    妙啊

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: [:show, :edit, :update, :destroy]
+  before_action :set_game, only: [:show, :edit, :update, ]
 
   # GET /games
   # GET /games.json
@@ -12,10 +12,7 @@ class GamesController < ApplicationController
   def show
   end
 
-  # GET /games/new
-  def new
-    @game = Game.new
-  end
+
 
   # GET /games/1/edit
   def edit
@@ -23,23 +20,7 @@ class GamesController < ApplicationController
 
   # POST /games
   # POST /games.json
-  def create
-    @game = Game.new(game_params)
-
-    respond_to do |format|
-      if @game.save
-        format.html { redirect_to @game, notice: 'Game was successfully created.' }
-        format.json { render :show, status: :created, location: @game }
-      else
-        format.html { render :new }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /games/1
-  # PATCH/PUT /games/1.json
-  def update
+   def update
     respond_to do |format|
       if @game.update(game_params)
         format.html { redirect_to @game, notice: 'Game was successfully updated.' }
@@ -51,15 +32,7 @@ class GamesController < ApplicationController
     end
   end
 
-  # DELETE /games/1
-  # DELETE /games/1.json
-  def destroy
-    @game.destroy
-    respond_to do |format|
-      format.html { redirect_to games_url, notice: 'Game was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+
 
   private
   # Use callbacks to share common setup or constraints between actions.
